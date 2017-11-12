b1312107c7ede264f4dbbae7c9306bab8cb56f63
kanaabe <kanakoabe5@gmail.com>
2017-11-12 00:44:47 -0500

    Add tracking pixel

--- desktop/apps/editorial_features/components/gucci/components/nav/header.jsx
+++ desktop/apps/editorial_features/components/gucci/components/nav/header.jsx
@@ -1,10 +1,11 @@
-import styled from 'styled-components'
-import PropTypes from 'prop-types'
-import React, { Component } from 'react'
 import { pMedia } from '@artsy/reaction-force/dist/Components/Helpers'
-import Icon from '@artsy/reaction-force/dist/Components/Icon'
 import { Fonts } from '@artsy/reaction-force/dist/Components/Publishing/Fonts'
 import { PartnerInline } from '../partner/partner_inline.jsx'
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import moment from 'moment'
+import PropTypes from 'prop-types'
+import Icon from '@artsy/reaction-force/dist/Components/Icon'
 
 export class Header extends Component {
   static propTypes = {
@@ -32,6 +33,17 @@ export class Header extends Component {
           url={partner_url}
           logo={partner_logo}
         />
+
+        {/* Gucci Tracking Pixel */}
+        <a href={`https://ad.doubleclick.net/ddm/jump/N32001.3019648ARTSY/B20483079.208849246;sz=1x1;ord${moment().unix()}=?`}>
+          <img
+            src={`https://ad.doubleclick.net/ddm/ad/N32001.3019648ARTSY/B20483079.208849246;sz=1x1;ord=${moment().unix()};dc_lat=;dc_rdid=;tag_for_child_directed_treatment=?`}
+            border='0'
+            width='1'
+            height='1'
+            alt='Advertisement'
+          />
+        </a>
 
         {!isMobile &&
           <div className='title'>
